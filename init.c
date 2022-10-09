60ae23d7bd24d0ffd523d78f692bbc2eb610148b
vhemeret <val.kawa@icloud.com>
2022-10-10 00:24:14 +0200

    new master

---
+++ init.c
@@ -0,0 +1,15 @@
+#include "zeltat.h"
+
+int init_SDL(t_init *SDL)
+{
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        return (0);
+    SDL->win = SDL_CreateWindow("Zeltat", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!SDL->win)
+        return (0);
+    SDL->renderer = SDL_CreateRenderer(SDL->win, -1, SDL_RENDERER_ACCELERATED);
+    if (!SDL->renderer)
+        return (0);
+    return (1);
+}
---
+++ zeltat.h
@@ -0,0 +1,35 @@
+#ifndef ZELTAT_H
+# define ZELTAT_H
+
+/********************* LIBRAIRIES *********************/
+
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h> 
+# include <stdio.h>
+# include <stdlib.h>
+
+/*********************** DEFINE ***********************/
+
+# define SCREEN_WIDTH 900
+# define SCREEN_HEIGHT 550
+# define PATH_MENU_PLAY "./img/play_menu.png"
+# define PATH_MENU_QUIT "./img/quit_menu.png"
+
+/********************* STRUCTURES *********************/
+
+typedef struct	s_init
+{
+	SDL_Window		*win;
+	SDL_Renderer    *renderer;
+    SDL_Surface     *menu_play;
+    SDL_Surface     *menu_quit;
+    SDL_Texture     *texture_menu_play;
+    SDL_Texture     *texture_menu_quit;
+
+} t_init;
+
+/********************* PROTOTYPES *********************/
+
+int	init_SDL(t_init *SDL);
+
+#endif
